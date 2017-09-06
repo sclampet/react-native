425441f5dc739eecf6b90896c0ed8cf3742bb263
Scott Clampet <scottclampet@Scotts-MacBook-Pro.local>
2017-09-06 15:15:55 -0600

    ignore node_modules

---
+++ fullApps/meetupApp/meetup-mobile/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { fetchMeetups } from './constants/api';
+
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      meetups: []
+    };
+
+  };
+
+  // static defaultProps = {
+  //   // fetchMeetups
+  // };
+
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    fetchMeetups()
+      .then((res) => {
+        this.setState({
+          loading: false,
+          meetups: res.meetups
+        });
+        console.log('meetups: ' + this.state.meetups);
+      });
+  };
+
+  render() {
+    const meetups = this.state.meetups.map((meetup, i) => {
+      return <Text key={i}>{meetup.title}</Text>
+    });
+
+    if(this.state.loading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator size='large' />
+        </View>
+      )
+    }
+    return (
+      <View style={styles.container}>
+        <Text>Meetup!</Text>
+        {meetups}
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- meetupApp/meetup-backend/server/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/meetupApp');
-
-mongoose.connection
-  .once('open', () => console.log('DB Connected!'))
-  .on('err', err => console.log(err));
