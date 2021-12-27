923ae160703dd579e8ef36da7a81fef5e08f5359
sclampet <clamptron@gmail.com>
2021-12-27 16:31:31 -0500

    chat-app | implement api and user model

--- chat-app/App.tsx
+++ chat-app/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useCachedResources from "./hooks/useCachedResources";
@@ -7,13 +7,65 @@ import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
 
 import { withAuthenticator } from "aws-amplify-react-native";
-import Amplify from "aws-amplify";
+import Amplify, { API, Auth, graphqlOperation } from "aws-amplify";
 import config from "./src/aws-exports";
+import { getUser } from "./src/graphql/queries";
+import { createUser } from "./src/graphql/mutations";
 Amplify.configure(config);
+
+const randomImages = [
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg",
+];
 
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const getRandomImage = () => {
+    return randomImages[Math.floor(Math.random() * randomImages.length)];
+  };
+
+  //Link user in Auth to Cognito: only when App is first mounted
+  useEffect(() => {
+    const fetchUser = async () => {
+      //get authenitcated user from Auth
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+
+      if (userInfo) {
+        //get the user from backend using the user's "sub"(unique indentifier) from Auth
+        const userData = await API.graphql(
+          graphqlOperation(getUser, {
+            id: userInfo.attributes.sub,
+          })
+        );
+
+        if (userData.data.getUser) {
+          console.log("User is already registered in db");
+          return;
+        }
+
+        const newUser = {
+          id: userInfo.attributes.sub,
+          name: userInfo.username,
+          imageUri: getRandomImage(),
+          status: "Hey, I am using this chat app!",
+        };
+
+        await API.graphql(
+          graphqlOperation(createUser, {
+            input: newUser,
+          })
+        );
+      }
+    };
+
+    fetchUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
