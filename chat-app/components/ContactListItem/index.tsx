09a9bdfe099264a03a91f32928cbce40a555c7ab
sclampet <clamptron@gmail.com>
2021-12-29 13:23:55 -0500

    chat-app | create chat room

--- chat-app/components/ContactListItem/index.tsx
+++ chat-app/components/ContactListItem/index.tsx
@@ -4,6 +4,11 @@ import { User } from "../../types";
 import moment from "moment";
 import styles from "./style";
 import { useNavigation } from "@react-navigation/native";
+import { API, Auth, graphqlOperation } from "aws-amplify";
+import {
+  createChatRoom,
+  createChatRoomUser,
+} from "../../src/graphql/mutations";
 
 export type ContactListItemProps = {
   user: User;
@@ -13,8 +18,48 @@ const ContactListItem = (props: ContactListItemProps) => {
   const navigation = useNavigation();
   const { user } = props;
 
-  const onPress = () => {
-    //navigate to chatroom with this
+  const onPress = async () => {
+    try {
+      //1. Create new ChatRoom
+      const newChatRoomData = await API.graphql(
+        graphqlOperation(createChatRoom, { input: {} })
+      );
+
+      if (!newChatRoomData.data) {
+        console.log("Failed to create new ChatRoom");
+        return;
+      }
+
+      const newChatRoom = newChatRoomData.data.createChatRoom;
+
+      //2. Add 'user' to ChatRoom
+      await API.graphql(
+        graphqlOperation(createChatRoomUser, {
+          input: {
+            userID: user.id,
+            chatRoomID: newChatRoom.id,
+          },
+        })
+      );
+
+      //3. Add authenticated user to ChatRoom
+      const userInfo = await Auth.currentAuthenticatedUser();
+      await API.graphql(
+        graphqlOperation(createChatRoomUser, {
+          input: {
+            userID: userInfo.attributes.sub,
+            chatRoomID: newChatRoom.id,
+          },
+        })
+      );
+
+      navigation.navigate("ChatRoom", {
+        id: newChatRoom.id,
+        name: "Harcoded Name",
+      });
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
